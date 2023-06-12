f18b57c4519b504600b8ba9c2fb58ca6aba12ba9
Your Name <you@example.com>
2023-06-12 20:02:06 +0700

    move to branch header

--- src/constants/PhoneConstant.ts
+++ src/constants/PhoneConstant.ts
@@ -1,3 +1,16 @@
+export const CATEGORY = [
+  { label: 'Điện thoại, Tablet', value: 1, to: '/phone' },
+  { label: 'Laptop', value: 2, to: '/laoptop' },
+  { label: 'Tai nghe', value: 3, to: '/tainghe' },
+  { label: 'Đồng hồ, Máy ảnh', value: 4, to: '/dongho&mayanh' },
+  { label: 'Phụ kiện', value: 5, to: '/phukien' },
+  { label: 'PC, Màn hình', value: 6, to: '/pc&manhinh' },
+  { label: 'Tivi', value: 7, to: '/tivi' },
+  { label: 'Hàng cũ', value: 8, to: '/hangcu' },
+  { label: 'Chương trình khuyến mãi', value: 9, to: '/khuyenmai' },
+  { label: 'Tin công nghệ', value: 10, to: '/tintuc' },
+];
+
 export const BRANDS = [
     { label: 'IPhone', value: 1, to: '/iphone' },
     { label: 'Samsung', value: 2, to: '/samsung' },

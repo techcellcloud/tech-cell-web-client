d916fa16cdf3f311e2ceb56f0993ed8f567aedd0
tiendx161202 <dangxuantien161202@gmail.com>
2023-06-29 00:22:57 +0700

    search__modal

--- src/components/Navigation/HeaderClient.tsx
+++ src/components/Navigation/HeaderClient.tsx
@@ -11,6 +11,7 @@ import {
     IconButton,
     Toolbar,
     Stack,
+    Button,
 } from '@mui/material';
 import { Menu as MenuIcon, ShoppingCart as ShoppingCartIcon } from '@mui/icons-material';
 import SearchIcon from '@mui/icons-material/Search';
@@ -18,7 +19,10 @@ import { useTheme } from '@mui/material/styles';
 import { MenuComponent, SearchComponent } from '@components/Form';
 import { DRAWER_WIDTH, NAV_ITEMS } from '@constants/NavContants';
 import { DrawerLayout } from '@components/Layout';
-// import styles from 'styles/components/header.module.scss';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import styles from 'styles/components/button.module.scss';
 
 interface Props {
     window?: () => Window;
@@ -35,6 +39,31 @@ export const HeaderClient = (props: Props) => {
 
     const container = window !== undefined ? () => window().document.body : undefined;
 
+    // Modal search
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const top100Films = [
+        { title: 'The Shawshank Redemption', year: 1994 },
+        { title: 'The Godfather', year: 1972 },
+        { title: 'The Godfather: Part II', year: 1974 },
+        { title: 'The Dark Knight', year: 2008 },
+        { title: '12 Angry Men', year: 1957 },
+        { title: "Schindler's List", year: 1993 },
+        { title: 'Pulp Fiction', year: 1994 },
+    ];
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '13%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '100%',
+        height: 'auto',
+        bgcolor: '#fff',
+        padding: '10px',
+    };
+
     return (
         <Box sx={{ display: 'flex', height: { xs: '56px', sm: '64px' } }}>
             <AppBar
@@ -44,14 +73,16 @@ export const HeaderClient = (props: Props) => {
                     display: 'flex',
                     justifyContent: 'space-between',
                     alignItems: 'center',
-                    maxWidth:'100%'
+                    maxWidth: '100%',
+                    padding: '0px',
                 }}
             >
                 <Toolbar
                     sx={{
-                        justifyContent: { xs: 'space-around', lg: 'space-between' },
-                        // width: { lg: '1200px', xs: '390px', md: '990px', sm: '900px' },
+                        justifyContent: { xs: 'space-between', lg: 'space-between' },
+                        width: { lg: '1200px', xs: '390px', md: '990px', sm: '900px' },
                         alignItems: 'center',
+                        padding: { xs: '0px 10px' },
                     }}
                 >
                     <IconButton
@@ -104,30 +135,37 @@ export const HeaderClient = (props: Props) => {
                             ))}
                         </Box>
                     </Stack>
-                    <Box sx={{ display:{ sm: 'flex', md: 'flex', lg: 'none' }}}>
-                        <SearchIcon />
-                        <Box sx={{marginLeft:'15px'}}><ShoppingCartIcon /></Box>
+                    <Box sx={{ display: { xs: 'flex', sm: 'flex', md: 'flex', lg: 'none' } }}>
+                        <Box onClick={handleOpen}>
+                            <SearchIcon />
+                        </Box>
+                        <Box sx={{ marginLeft: '15px' }}>
+                            <ShoppingCartIcon />
+                        </Box>
                     </Box>
                 </Toolbar>
-                {/* <Box
-                    sx={{
-                        display: { xs: 'flex', lg: 'none' },
-                        justifyContent: 'center',
-                        alignContent: 'center',
-                    }}
+                {/* Modal */}
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
                 >
-                    <Box
-                        sx={{
-                            color: '#fff',
-                            backgroundColor: '#F16464',
-                            width: '320px',
-                            margin: '5px 0px 5px 0px',
-                            borderRadius: '5px',
-                        }}
-                    >
-                        <SearchComponent />
+                    <Box sx={style}>
+                        <Stack sx={{ maxWidth: '100% ' }}>
+                            <Autocomplete
+                                id="free-solo-demo"
+                                freeSolo
+                                options={top100Films.map((option) => option.title)}
+                                renderInput={(params) => <TextField {...params} label="Tìm kiếm" />}
+                            />
+                        </Stack>
+
+                        <Box sx={{ display: 'flex' }}>
+                            <button className={styles.button_search}>Tìm kiếm</button>
+                        </Box>
                     </Box>
-                </Box> */}
+                </Modal>
             </AppBar>
             <Box component="nav" sx={{}}>
                 <Drawer
